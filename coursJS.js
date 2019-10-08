17b062dd3157f9f4587861ca859710265b09672d
AnasAzzouzi <haitham.azzouzi@gmail.com>
2019-10-08 16:38:06 +0100

    v1

---
+++ coursJS.js
@@ -0,0 +1,40 @@
+/*let x=5;
+let y=6; 
+//alert(x+y);
+let age=50;
+const pi=3.14;
+
+console.log(`j'ai ${age} ans
+  );
+x=x?alert(1):alert(2);
+let rep=prompt("donner un nombre");
+Number.parseInt(rep)>=10?alert("Nje7ti :)"):alert("s9eti :(");*/
+
+/*let val1= Number.parseInt(prompt("donner la 1ere valeur :"));
+let op = prompt("donner l'operation ");
+let val2=Number.parseInt(prompt("donner la 2eme valeur :"));
+
+
+switch(op){
+	case "+" : alert(`${val1}${op}${val2}= ${val1+val2}`);break;
+	case "-" : alert(`${val1}${op}${val2}= ${val1-val2}`);break;
+	case "*" : alert(`${val1}${op}${val2}= ${val1*val2}`);break;
+	case "/" : if (val2=="0" ) alert(" division par 0"); else alert(`${val1}${op}${val2}= ${val1/val2}`);break;
+	default : alert("non valid operation");
+}*/
+
+/* function Somme( x ,y ){
+
+ 	if( Number.isInteger(x)&&Number.isInteger(y))
+ 		return x+y;
+ 	else return NaN;
+ }
+
+ alert(Somme("lol",3));*/
+
+ let f= function(a,b){
+ 	
+ 	console.log(a+b);
+ };
+
+ f(1,u);
